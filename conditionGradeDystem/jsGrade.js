006717e6426496934fe539f3c655deed62dedece
Mohammodrubel <74967048+mohammodrubel@users.noreply.github.com>
2021-08-05 16:43:03 +0600

    Add files via upload

---
+++ conditionGradeDystem/jsGrade.js
@@ -0,0 +1,22 @@
+var result = 3
+
+    if (result >=80){
+        console.log('you have got A+')
+    }
+    else if (result >=70){
+        console.log('you have got A')
+    }
+    else if (result >=60){
+        console.log('you have got A-')
+    }
+    else if (result >=50){
+        console.log('you have got B')
+    }
+    else if (result >=40){
+        console.log('you have got C')
+    }
+    else if (result >=33){
+        console.log('you have got D')
+    }else {
+        console.log('you are faill stupid')
+    }
